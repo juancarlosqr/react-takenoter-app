64bafa8a2a88eb954571060bdd6be118763b1451
Juan Carlos Quintero <jcquintero.ce@gmail.com>
2015-07-05 11:53:13 -0300

    add the "adding new note" feature

--- app/components/Notes/Notes.js
+++ app/components/Notes/Notes.js
@@ -1,16 +1,19 @@
 var React = require('react');
 var NotesList = require('./NotesList');
+var AddNote = require('./AddNote');
 
 var Notes = React.createClass({
   propTypes: {
     username: React.PropTypes.string.isRequired,
-    notes: React.PropTypes.array.isRequired
+    notes: React.PropTypes.array.isRequired,
+    addNote: React.PropTypes.func.isRequired
   },
   render: function () {
     return (
       <div>
         <h2>Notes</h2>
         <h3>{this.props.username}</h3>
+        <p><AddNote username={this.props.username} addNote={this.props.addNote} /></p>
         <p><NotesList notes={this.props.notes} /></p>
       </div>
     )
